f569bb2f33813131eb8f30c1051eaf41f4bcd9b7
cekane <ckane144@gmail.com>
2016-07-13 12:19:53 -0400

    Moved form to examples. Editor and Viewer are now empty.

---
+++ examples/Form/Editor/index.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { stable, underline, fieldName, tdRight } from './Editor.scss'
+import { Button } from '../Button'
+import { FormRow } from './FormRow'
+
+export class Editor extends React.Component{
+  constructor (props) {
+    super(props);
+  }
+
+  addField(name){ 
+    var uuid = require('node-uuid')
+    var id = uuid.v4()
+    var newFields = this.props.data.fields
+    var toAdd = { name: name, req: false, id: id }
+    newFields.push(toAdd)
+    this.props.data.fields = newFields
+    var obj = { target: { value: newFields } }
+    this.props.handleChange('this.props.data.fields')(obj)
+    console.log(this.props.data.fields)
+  }
+
+  removeField(id){
+    var newFields = this.props.data.fields.filter(k=>{
+      if(id !== k.id)
+        return k
+    })
+    this.props.data.fields = newFields
+    var obj = { target: { value: newFields } }
+    this.props.handleChange('this.props.data.fields')(obj)
+  }
+
+  render(){
+    const that = this
+    console.log("PROPS", this.props.data)
+    if (!this.props.data.fields)
+    {
+      this.props.data.fields = []
+    }
+    if (!this.props.data.email){
+      this.props.data.email = ''
+    }
+    console.log('EMAIL', this.props.data.email)
+    return(
+      <span>
+        
+        <li>
+          <span>Your Email</span>
+          <input
+            className="form-control" 
+            type="email"
+            value={ this.props.data.email }
+            onChange={ this.props.handleChange('data.email') }/>
+        </li>
+        <li>
+          <span>Add Fields</span>
+          <span className = { fieldName }>
+            <input
+              className = "form-control" 
+              type = "text"
+              ref = "field"
+              onKeyPress = { 
+              k=>{
+                  if(k.key === 'Enter')
+                  {
+                    that.addField(that.refs.field.value)
+                    that.refs.field.value = ''
+                  }   
+                } 
+              }
+            />
+            <Button type="good" small solid onClick={ 
+                  function(){
+                    that.addField(that.refs.field.value)
+                    that.refs.field.value = ''
+                  } 
+                }>
+              +
+            </Button>
+          </span>
+        </li>
+        <li>
+          <span>Visable Fields</span>
+          <table className={ stable }>
+            <tr>
+              <td >
+                Fields
+              </td>
+              <td className={ tdRight }>
+                (Required)
+              </td>
+            </tr>
+              {
+                this.props.data.fields.map((k, i)=>{
+                  return(
+                      <FormRow 
+                        text={ k.name } 
+                        onChangeRequired={ this.props.handleChange( `data.fields.${ i }.req` ) }
+                        require={ k.req } 
+                        onClick={ function(){
+                          that.removeField(k.id)
+                        } }
+                      />
+                    )
+                  }
+                )
+              }
+          </table>
+        </li>
+      </span>
+    )
+  }
+}
--- src/components/Viewer/index.js
+++ src/components/Viewer/index.js
@@ -1,77 +1,18 @@
 import React from 'react'
 import { Button } from '../Button'
 import { Select, Case } from '../Select'
-import { frmInput } from './Viewer.scss'
+import {  } from './Viewer.scss'
 
 import className from 'classnames'
 
 export class Viewer extends React.Component{
   constructor (props) {
     super(props);
-    this.state = {
-      values: { },
-      visObjs: { },
-      errorMsg: ' ',
-      sub: 'Submit'
-    }
-  }
-
-  validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-
-  _setValue (key) {
-    const that = this
-    return function(e){
-      var newState = that.state;
-      newState.values[key] = e.target.value;
-      console.log(newState.values[key])
-      that.setState(newState); 
-    }
-  }
-
-  visibleFields(fields)
-  {
-    var flds = fields
-    return flds.map(k =>{
-      var asterix
-      if (k.req) 
-        asterix = ' (Required)'
-      else
-        asterix = '' 
-      return (  <div>{k.name + asterix}
-                  <input 
-                    className={ className("form-control", frmInput) } 
-                    type="text"  
-                    placeholder={ k.name }
-                    onChange={ this._setValue(k.name) }/>
-                </div>)
-    }) 
   }
 
   render(){
-    const that = this
     return (
-      <Select on={ this.validateEmail(this.props.data.email) }>
-        <Case when={ false }>
-          <div>INVALID EMAIL</div>
-        </Case>
-        <Case when={ true }>
-          <div>
-            {console.log("VALIDATE EMAIL", this.validateEmail(this.props.data.email))}
-            {this.visibleFields(this.props.data.fields)}
-            <p>{ this.state.errorMsg }</p>
-            <Button  type="good" onClick={
-              function(){
-                  console.log("You would have submited stuff")
-                }
-              }>
-              <h4>Submit</h4>
-            </Button>
-          </div>
-        </Case>
-      </Select>
+      <div>In Viewer</div>
     )
   }
 }
